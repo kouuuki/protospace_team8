c08df2d525ceb31e219a12bc58ef2d2f54df755e
kohei-isoda <isodakohei0511@gmail.com>
2018-03-04 14:47:04 +0900

    画像アップロードによるエラー解決

--- app/uploaders/prototype_image_uploader.rb
+++ app/uploaders/prototype_image_uploader.rb
@@ -4,7 +4,7 @@ class PrototypeImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -36,15 +36,15 @@ class PrototypeImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [800, 500]
   end
   #
-  # version :sub_thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :sub_thumb do
+    process :resize_to_fit => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
